809b62f410128a19c85cab4eace719cb34b11e60
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-08-28 15:52:15 +0530

    Feature: Basic Express app with mongoose setup done

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const env = require('dotenv');
+const mongoose = require('mongoose');
+
+env.config();
+const app = express(); // express app object
+
+// configuring body parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/home', (req, res) => {
+    console.log("Hitting /home");
+    return res.json({
+        success: true,
+        message: 'Fetched home'
+    });
+});
+
+app.listen(process.env.PORT, async () => {
+    // this callback gets execcuted, once we successfully start the server on the given port
+    console.log(`Server started on Port ${process.env.PORT} !!`);
+
+    try {
+        await mongoose.connect(process.env.DB_URL); // connected to the mongo server
+        console.log("Successfully connected to mongo");
+    } catch (err) {
+        console.log("Not able to connect mongo", err);
+    }
+});
