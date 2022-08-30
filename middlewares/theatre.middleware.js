14cd870f2c347a40a284be8a631eca567f8bdc49
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-08-31 00:06:29 +0530

    Feat: Theatre create api validators added

---
+++ middlewares/theatre.middleware.js
@@ -0,0 +1,23 @@
+const { errorResponseBody } = require('../utils/responsebody');
+const validateTheatreCreateRequest = async (req, res, next) => {
+    // validate the presence of name
+    if(!req.body.name) {
+        errorResponseBody.message = "The name of the theatre is not present in the request";
+        return res.status(400).json(errorResponseBody)
+    }
+    // validation for the presence of pincode
+    if(!req.body.pincode) {
+        errorResponseBody.message = "The pincode of the theatre is not present in the request";
+        return res.status(400).json(errorResponseBody);
+    }
+    // validation for the presence of city
+    if(!req.body.city) {
+        errorResponseBody.message = "The city of the theatre is not present";
+        return res.status(400).json(errorResponseBody);
+    }
+    next(); // everything is fine move to the next middleware
+}
+
+module.exports = {
+    validateTheatreCreateRequest
+}
--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -1,8 +1,10 @@
 const theatreController = require('../controllers/theatre.controller');
+const theatreMiddleware = require('../middlewares/theatre.middleware');
 
 const routes = (app) => {
     app.post(
         '/mba/api/v1/theatres',
+        theatreMiddleware.validateTheatreCreateRequest,
         theatreController.create
     );
 }
