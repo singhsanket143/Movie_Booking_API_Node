1155ace4458f3caa62520a0f167ac3c051cb57c9
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-09-01 22:10:42 +0530

    Feat: Added reference to the movie collection inside theatre resource

--- models/theatre.model.js
+++ models/theatre.model.js
@@ -18,7 +18,11 @@ const theatreSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
-    address: String
+    address: String,
+    movies: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'Movie'
+    }
 }, {timestamps: true});
 
 const Theatre = mongoose.model('Theatre', theatreSchema);
