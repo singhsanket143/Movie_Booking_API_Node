35e0283014b17a5fe1b45210cdd0705651a23761
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-09-07 18:53:34 +0530

    Feat: Added user Schema

---
+++ models/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email'],
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    userRole: {
+        type: String,
+        required: true,
+        default: "CUSTOMER"
+    },
+    userStatus: {
+        type: String,
+        required: true,
+        default: "APPROVED"
+    }
+}, {timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
