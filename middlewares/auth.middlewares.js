4e260c4b96b7623ccaf392d3b1dc23cc4ee49484
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-09-08 20:21:31 +0530

    Feat: added isauthenticated middleware for checking api auth

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -1,4 +1,7 @@
+const jwt = require('jsonwebtoken');
+
 const { errorResponseBody } = require('../utils/responsebody');
+const userService = require('../services/user.service');
 
 /**
  * validator for user signup
@@ -52,8 +55,38 @@ const validateSigninRequest = async (req, res, next) => {
     next();
 }
 
+const isAuthenticated = async (req, res, next) => {
+    try {
+        const token = req.headers["x-access-token"];
+        if(!token) {
+            errorResponseBody.err = "No token provided";
+            return res.status(403).json(errorResponseBody);
+        }
+        const response = jwt.verify(token, process.env.AUTH_KEY);
+        if(!response) {
+            errorResponseBody.err = "Token not verified";
+            return res.status(401).json(errorResponseBody);
+        }
+        const user = await userService.getUserById(response.id);
+        req.user = user.id;
+        next();
+    } catch (error) {
+        if(error.name == "JsonWebTokenError") {
+            errorResponseBody.err = error.message;
+            return res.status(401).json(errorResponseBody);
+        }
+        if(error.code == 404) {
+            errorResponseBody.err = "User doesn't exist"
+            return res.status(error.code).json(errorResponseBody);
+        }
+        errorResponseBody.err = error;
+        return res.status(500).json(errorResponseBody);
+    }
+}
+
 
 module.exports = {
     validateSignupRequest,
-    validateSigninRequest
+    validateSigninRequest,
+    isAuthenticated
 }
--- services/user.service.js
+++ services/user.service.js
@@ -46,7 +46,21 @@ const getUserByEmail = async (email) => {
     }
 }
 
+const getUserById = async (id) => {
+    try {
+        const user = await User.findById(id);
+        if(!user) {
+            throw {err: "No user found for the given id", code: 404};
+        }
+        return user;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
 module.exports = {
     createUser,
-    getUserByEmail
+    getUserByEmail,
+    getUserById
 }
