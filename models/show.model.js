0ba3806d8655e5245ae2e57d941269ace0e4e34b
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-09-19 17:24:54 +0530

    Added show model

---
+++ models/show.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const showSchema = new mongoose.Schema({
+    theatreId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    movieId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    timing: {
+        type: String,
+        required: true
+    },
+    noOfSeats: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    format: {
+        type: String
+    }
+}, {timestamps: true});
+
+const Show = mongoose.model('Show', showSchema);
+
+module.exports = Show;
