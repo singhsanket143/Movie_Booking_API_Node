57aaacd3a9b0f8d88f1aa5f5cc24adf47267a465
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-09-22 12:32:18 +0530

    Feat: added seat reduction logic and price calc logic

--- services/booking.services.js
+++ services/booking.services.js
@@ -1,9 +1,19 @@
 const Booking = require('../models/booking.model');
+const Show = require('../models/show.model');
+
 const { STATUS } = require('../utils/constants');
 
 const createBooking = async (data) => {
     try {
+        const show = await Show.findOne({
+            movieId: data.movieId, 
+            theatreId: data.theatreId, 
+            timing: data.timing
+        });
+        console.log(show.price, data.noOfSeats);
+        data.totalCost = data.noOfSeats*show.price;
         const response = await Booking.create(data);
+        await show.save();
         return response;
     } catch (error) {
         console.log(error);
--- services/payment.service.js
+++ services/payment.service.js
@@ -1,11 +1,17 @@
 const Payment = require('../models/payment.model');
 const Booking = require('../models/booking.model');
+const Show = require('../models/show.model');
 const User = require('../models/user.model');
 const { STATUS, BOOKING_STATUS, PAYMENT_STATUS, USER_ROLE } = require('../utils/constants');
 
 const createPayment = async (data) => {
     try {
         const booking = await Booking.findById(data.bookingId);
+        const show = await Show.findOne({
+            movieId: booking.movieId,
+            theatreId: booking.theatreId,
+            timing: booking.timing
+        });
         if(booking.status == BOOKING_STATUS.successfull) {
             throw {
                 err: 'Booking already done, cannot make a new payment against it',
@@ -44,6 +50,8 @@ const createPayment = async (data) => {
         }
         payment.status = PAYMENT_STATUS.success;
         booking.status = BOOKING_STATUS.successfull;
+        show.noOfSeats -= booking.noOfSeats;
+        await show.save();
         await booking.save();
         await payment.save();
         return booking;
